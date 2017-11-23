e4724b561ad2a00f521940de882a3f437234da44
Akin Oluyele <waleander@gmail.com>
2017-11-23 07:38:58 +0100

    add functions for search

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -264,4 +264,27 @@
             });
         })
     });
+
+    var $mainNavSearchBtn = $('#main-nav-search-btn');
+    var $searchModal = $('#search-modal');
+    var $searchCloseBtn = $('#search-close-btn');
+    var $content = $('.content');
+    var $modalSearchField = $('#modal-search-field');
+    var $searchTags = $('.search-tag');
+
+    $mainNavSearchBtn.on('click', function() {
+        $searchModal.addClass('active');
+        $content.css('transform', 'scale(.7)');
+        $modalSearchField[0].focus();
+    });
+
+    $searchCloseBtn.on('click', function() {
+        $content.css('transform', 'scale(1)');
+        $searchModal.removeClass('active');
+        $modalSearchField.val('');
+    });
+
+    $searchTags.on('click', function() {
+        $modalSearchField.val($(this).text());
+    });
 }(window, jQuery));
