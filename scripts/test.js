de538956c7a8ff521d2f26fac1e0dea6c0d04ae2
Akin Oluyele <waleander@gmail.com>
2017-12-03 09:28:46 +0100

    make changes to test

--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,6 @@
 var expect = require("chai").expect
 var should = require("chai").should()
-var addTwo = require("../index").addTwo
+var addTwo = require("../index.js").addTwo
 describe("Test the behavior of addTwo()", function() {
     it('should return 2 when given 1 and 1 via expect()', function() {
         expect(addTwo(1, 1)).to.be.equal(2)
